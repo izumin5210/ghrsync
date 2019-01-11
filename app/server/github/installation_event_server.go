83d40500633733131e73ffb914a5791323736a4b
izumin5210 <m@izum.in>
2019-01-12 01:14:39 +0900

    grapi g service github/installation_event create

---
+++ app/server/github/installation_event_server.go
@@ -0,0 +1,30 @@
+package github
+
+import (
+	"context"
+
+	"github.com/izumin5210/grapi/pkg/grapiserver"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	github_pb "github.com/izumin5210/ghrsync/api/github"
+)
+
+// InstallationEventServiceServer is a composite interface of github_pb.InstallationEventServiceServer and grapiserver.Server.
+type InstallationEventServiceServer interface {
+	github_pb.InstallationEventServiceServer
+	grapiserver.Server
+}
+
+// NewInstallationEventServiceServer creates a new InstallationEventServiceServer instance.
+func NewInstallationEventServiceServer() InstallationEventServiceServer {
+	return &installationEventServiceServerImpl{}
+}
+
+type installationEventServiceServerImpl struct {
+}
+
+func (s *installationEventServiceServerImpl) CreateInstallationEvent(ctx context.Context, req *github_pb.CreateInstallationEventRequest) (*github_pb.InstallationEvent, error) {
+	// TODO: Not yet implemented.
+	return nil, status.Error(codes.Unimplemented, "TODO: You should implement it!")
+}
